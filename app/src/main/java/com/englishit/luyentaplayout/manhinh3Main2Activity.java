15674697183117d13c9fb861fc5756aa11276637
trankimhoang1998 <trankimhoang1998@gmail.com>
2018-09-14 22:19:17 +0700

    Initial commit

---
+++ app/src/main/java/com/englishit/luyentaplayout/manhinh3Main2Activity.java
@@ -0,0 +1,13 @@
+package com.englishit.luyentaplayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class manhinh3Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manhinh3_main2);
+    }
+}
